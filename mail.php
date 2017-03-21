fda0c484e5a075de6978bb0f9ff3d240a8c6c66c
LismaJena <lismajena@gmail.com>
2017-03-21 15:20:07 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,18 @@
+﻿<?php 
+if(isset($_POST['submit'])){
+    $to = "lismajena@gmail.com";
+    $from = $_POST['email']; 
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $subject = "Order Confirmation";
+    $subject2 = "Order Confirmation";
+    $message = "Order has been placed";
+    $message2 = "Your order has been confirmed";
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); 
+    
+    }
+?>
